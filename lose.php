142bfa63da3df2fc9e9f66ee0523a4eb221e258d
JaviSanjuanHipolito <sanjuanhipolitojavier@gmail.com>
2025-11-09 21:58:57 +0100

    Añado lose.php

---
+++ lose.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Has perdido!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="game-container">
+        <h1>¡Lo siento! Has perdido 😢</h1>
+        <button onclick="window.location.href='index.php'">Jugar de nuevo</button>
+    </div>
+</body>
+</html>
