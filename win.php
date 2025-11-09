a68285d51a14f2ac0e584fa086da60764f8eba40
JaviSanjuanHipolito <sanjuanhipolitojavier@gmail.com>
2025-11-09 21:58:36 +0100

    Añado win.php

---
+++ win.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Has ganado!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="game-container">
+        <h1>¡Felicidades! Has ganado 🎉</h1>
+        <button onclick="window.location.href='index.php'">Jugar de nuevo</button>
+    </div>
+</body>
+</html>
